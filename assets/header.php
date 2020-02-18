88b5fb43054bd42b66a32087da118faded901196
y.dai <y.dai@>
2020-02-18 11:57:58 +0100

    inipush

---
+++ assets/header.php
@@ -0,0 +1,19 @@
+<div class="navi">
+	<ul>
+		<li>
+			<a href="#ptop">To Bottom</a>
+		</li>
+		<li>
+			<a href="http://www.orithegame.com">To Nibel</a>
+		</li>
+		<li class="recall">
+			<a href="#fakebot">Recall our logo!</a>
+		</li>
+		<li id="showbg">
+			Show background
+		</li>
+		<li id="hidebg">
+			Hide background
+		</li>
+	</ul>
+</div>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Test</title>
+	</head>
+	<body>
+		<h1>Title test</h1>
+		<?php include('assets/header.php'); ?>
+	</body>
+</html>
