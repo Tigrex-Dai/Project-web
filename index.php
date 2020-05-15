db3a7005bae999c72ba9f7c01c963f9f6bd8c096
DAI YUQUAN <y.dai@etu.unistra.fr>
2020-05-15 15:06:13 +0200

    Final update

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include "config.php"; ?>
+
 <!DOCTYPE HTML>
 <html lang="fr">
 <html>
@@ -7,37 +9,103 @@
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title>Test</title>
 		<a name="faketop"></a>
-		<h1>Title test</h1>
+
 		<link href="templates/css/style.css" rel="stylesheet" type="text/css" >
+        <link href="templates/css/grid.css" rel="stylesheet" type="text/css" >
+        <link href="templates/css/button.css" rel="stylesheet" type="text/css" >
+        <script src="templates/code/code.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 	</head>
 	<body>
+        <div class="banner">
+            <div class="col_6">
+                <br/>
+                <br/>
+                <h1>Information Board</h1>
+                <h1>For</h1>
+                <h1>International</h1>
+                <h1>Students</h1>
+            </div>
+            <div class="col_6">
+                <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                <p><?php echo $lang['intro1'] ?></p>
+                <p><?php echo $lang['intro2'] ?></p>
+            </div>
+        </div>
 		<?php include('templates/sidebar.php'); ?>
-		<?php include('templates/language.php'); ?>
+		<div class="lang">
+			<ul>
+				<li>
+					<a href="index.php?lang=fr">Français</a><span><img src="templates/fr.png" width="15px" height="15px"></span>
+				</li>
+				<li>
+					<a href="index.php?lang=en">English</a><span><img src="templates/en.png" width="15px" height="15px"></span>
+				</li>
+                <li>
+					<a href="index.php?lang=zh">中文</a><span><img src="templates/zh.png" width="15px" height="15px"></span>
+				</li>
+			</ul>
+       </div>
 		<div class="main">
-			<div class="showcase">
-				<span>This is a test box.</span>
-				<span>This is another test box.</span>
-				
-			</div>
-			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dapibus, felis vitae ultrices luctus, lacus lectus iaculis libero, quis porttitor orci velit placerat arcu. In mauris orci, luctus non elementum eu, imperdiet a sem. Aenean tempus, tortor elementum aliquet rutrum, diam magna aliquam ante, ac eleifend sem nibh vel mi. Curabitur molestie magna nisl, in scelerisque ante condimentum vel. Praesent lobortis massa ut arcu mattis, sed bibendum ipsum aliquam. Mauris auctor tincidunt mi, vel lobortis mi laoreet non. In vulputate sodales tortor. Morbi leo elit, porta nec lobortis id, aliquet a augue. Suspendisse rhoncus ligula eu mauris pharetra aliquam. Vestibulum quam diam, vulputate sit amet magna vitae, gravida ultricies lorem. Proin vitae magna sit amet arcu eleifend imperdiet at eu leo. Sed at facilisis enim. Integer elementum, lectus eget ornare ultrices, sem nibh lacinia sem, a commodo dolor libero quis risus. </p>
-			<p> Suspendisse gravida nunc et felis vehicula euismod. Nam fermentum felis quis arcu euismod porta. Aliquam sed viverra ante, at tincidunt tellus. Mauris nec dapibus erat. Phasellus eget enim sed lacus finibus rutrum ut eu nulla. Sed pretium venenatis urna, a elementum quam pulvinar vitae. Nunc in tristique nunc. Duis rutrum ultricies purus, vitae posuere arcu consectetur ac. </p>
-			<p> Maecenas scelerisque arcu sollicitudin, sodales ipsum nec, fermentum massa. Pellentesque viverra imperdiet tempus. Vivamus pharetra luctus tellus, pretium mollis tortor. Phasellus hendrerit est non ligula cursus dapibus. Ut ornare erat eget sagittis ornare. Suspendisse placerat pharetra mauris. Integer facilisis, nulla sit amet vestibulum interdum, diam nibh lobortis enim, sed elementum massa urna sed eros. </p>
-			<p> Nullam rhoncus mollis libero, at lacinia velit ullamcorper in. Nunc et viverra libero, sed imperdiet massa. Nunc accumsan ac mauris in rutrum. Aenean congue leo est, et viverra arcu consequat vitae. Nullam consectetur at quam nec sollicitudin. Duis et quam sollicitudin, molestie leo sed, consequat tellus. Cras porta nulla odio, dignissim porta magna sollicitudin a. </p>
-			<p> Integer tempus elit at aliquet lobortis. Donec eu ex eget nibh ornare vulputate. Cras in ex quis felis consectetur vestibulum ac non dolor. Integer et placerat ligula, ut semper diam. Praesent pulvinar luctus viverra. Mauris pulvinar vel leo at faucibus. Sed dignissim dui quis velit commodo, a suscipit ante finibus. Quisque iaculis, mauris volutpat egestas iaculis, orci orci fringilla erat, sit amet interdum augue odio nec ipsum. Etiam eu consectetur est. Phasellus sit amet metus sit amet erat ornare dictum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent quis elementum elit.  </p>
-			<p>This past weekend, Gareth Coker was part of Game Music Festival PL’s inaugural concert held in Wrocław, Poland. He wrote about his experience on his Facebook page and we thought we would share it with everyone here as well. Without further ado, here is Gareth’s recount of his incredible weekend:</p>
-			<p>“Woke up this morning with that bittersweet feeling. This weekend at the Game Music Festival in Poland representing Moon Studios and Ori the Game will be a moment that I cherish forever. Don’t really want to leave but all good things must come to an end!</p>
-			<p>Considering that this is the 1st edition of this festival, I am stunned at how well they’ve pulled everything off. I have been blown away at the hospitality of everyone here. I can’t thank the organizers enough for making me – and I’m sure the other composers involved – feel so welcome, and putting us so at ease. I’ve even been able to do required music work while out here due to how well managed the event was.</p>
-			<p>Regarding the concert itself: a lot of composers don’t get to perform their work very often. In my case I’m usually in the studio control room and then moving on quickly to the next work. This was actually my first time performing my own work on stage (as opposed to a recording studio) with an orchestra both as a pianist and a conductor. I struggle greatly listening to my own past work, all I hear is flaws, but being part of the performance allowed me to reconnect with the soul of Ori, particularly conducting the premiere of music from a certain scene in Ori and the Will of the Wisps. It’s been a while since I’ve been this uplifted by something and I can’t wait to take the momentum from this week and put it into my upcoming work. Additionally, being able to share this with my parents was a priceless gift from the festival.</p>
-			<p>Perhaps most important was the opportunity to connect with so many fans of Ori. It’s always a pleasure to talk to people one-on-one about the game and also to feel their enthusiasm for Will of the Wisps. Thanks to everyone who came from both near and far for attending and supporting the event.</p>
-			<p>Given the success of the 4 concerts and multiple masterclasses that took place over the 2 days here, I can’t imagine there not being a second (and more) edition of this event. Any composer approached to be part of this festival should have zero hesitation in joining. It’s only going to improve. With the quality of the arrangers and orchestrators working here, they can feel safe handing over their music to be arranged for the various ensembles. Thanks to Jan, Aruto, David and Robert for their terrific work on Symphony of the Forest. Ori is an incredibly challenging soundtrack to translate to the concert hall becuase of all the prerecorded and synthetic elements in the music. However, the concept of an Ori concert is now proven, and has huge potential and room for improvement for further performances down the line.</p>
-			<p>I’ve been here 5 days, and it’s been so awesome making so many new friends here as well as being able to reconnect with existing ones, and also to meet several esteemed colleagues, Peter, Neal, Borislav, Arkadiusz and more. And thanks to Blazej, Ela, Jan and Marta (and her ‘minions’!) for introducing to me the delights of Polish vodka, and the…. strength of Polish drinking, amongst many other things of course! Wroclaw has been a great introduction to Poland, I’m told I should visit Krakow ASAP.</p>
-			<p>I can’t wait to come back, either as a performer or simply as a fan. I’ll be very surprised if this event doesn’t go from strength to strength. Whether there is one or a hundred of these, this will always be a treasured memory.</p>
-			<a name="fakebot"></a>
+            <br/>
+            <h2><?php echo $lang['pop'] ?></h2>
+            <div class="showcase">
+                <div class="col_4">
+                    <div class="pic"><img src="templates/pic01.jpg" width="300px" height="200px"></div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.</p>
+                    <div class="linklst"><a href="list.php"><?php echo $lang['read'] ?></a></div>
+                </div>
+                <div class="col_4">
+                    <div class="pic"><img src="templates/pic02.jpg" width="300px" height="200px"></div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.</p>
+                    <div class="linklst"><a href="list.php"><?php echo $lang['read'] ?></a></div>
+                </div>
+                <div class="col_4">
+                    <div class="pic"><img src="templates/pic03.jpg" width="300px" height="200px"></div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.</p>
+                    <div class="linklst"><a href="list.php"><?php echo $lang['read'] ?></a></div>
+                </div>
+            </div>
+            <a name="tolst"></a>
+            <div>&nbsp</div>
+            <div class="tolist">
+                <br/><br/>
+                <h2><?php echo $lang['list1'] ?></h2>
+                <h2><?php echo $lang['list2'] ?></h2>
+            </div>
+            <div class="lstbutton"><a href="list.php"><span><?php echo $lang['tolist'] ?></span></a></div>
+            <a name="toform"></a>
+            <div>&nbsp</div>
+            <div class="tolist">
+                <br/><br/>
+                <h2><?php echo $lang['form1'] ?></h2>
+                <h2><?php echo $lang['form2'] ?></h2>
+            </div>
+            <div class="lstbutton"><a href="form.php"><span><?php echo $lang['toform'] ?></span></a></div>
+
+            <br/><br/><br/><br/>
+
+            <div class="bottom">
+                <div class="col_6"><?php include('assets/schedule.php'); ?></div>
+                <div class="col_6">
+                    <span class="contact">
+                        <ul class="alt">
+                            <li>Telephone: 0123234345</li>
+                            <li>E-mail: contact@ifis.org</li>
+                            <li>Twitter: @Ifis_web</li>
+                            <li>Facebook: @Ifis_web</li>
+                        </ul>
+                    </span>
+                </div>
+            </div>
+
+            <a name="fakebot"></a>
+
 		</div>
-		
-		<?php include('assets/schedule.php'); ?>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="templates/code/code.js"></script>
-		
+
+
 	</body>
 </html>
+
+
