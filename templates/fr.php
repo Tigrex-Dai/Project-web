0bcf2089f45981e6f9dc0db6fb93ddb6adf5b1f5
DAI YUQUAN <y.dai@etu.unistra.fr>
2020-05-15 15:10:09 +0200

    Final update

---
+++ templates/fr.php
@@ -0,0 +1,27 @@
+<?php
+    $lang = array(
+        "intro1" => "Bienvenue sur notre panneau d'information,",
+        "intro2" => "assurez-vous de ne manquer aucune activité ici!",
+        "bt1" => "Vers bas",
+        "bt2" => "Vers Top",
+        "bt3" => "Vers Ernest",
+        "bt4" => "Liste d'activités",
+        "bt5" => "Créer une énouncé",
+        "pop" => "Voici quelques activités populaires ...",
+        "read" => "Lire la suite",
+        "list1" => "Vous n'avez pas trouvé d'activités qui vous intéressent?",
+        "list2" => "Appuyez simplement sur ce bouton!",
+        "tolist" => "Afficher toutes les activités",
+        "form1" => "Vous souhaitez annoncer une activité de vous-même?",
+        "form2" => "Appuyez ensuite sur ce bouton!",
+        "toform" => "Créer une énouncé",
+        "plan" => "De nouveaux plans ici",
+        "addp" => "Ajouter",
+        "ptitle1" => "Post une nouvelle énouncé",
+        "ptitle2" => "Enregistrer votre nom, prénom et adresse e-mail, et detailler les infos de votre enonce.",
+        "bkname" => "Nom et prénom",
+        "bkmail" => "E-mail",
+        "bkintro" => "Notez votre annonce avec la date et le lieu précis.",
+        "act" => "Activités"
+    );
+?>
