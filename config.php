087f6eccc40abe7f6dea59d7f4d7015f17de5faa
DAI YUQUAN <y.dai@etu.unistra.fr>
2020-05-15 15:04:14 +0200

    Final update

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION['lang']))
+        $_SESSION['lang'] = "en";
+    else if(isset($_GET['lang']) && $_SESSION['lang'] != $_GET['lang'] && !empty($_GET['lang']))
+        if($_GET['lang'] == "en")
+            $_SESSION['lang'] = "en";
+        else if($_GET['lang'] == "fr")
+            $_SESSION['lang'] = "fr";
+        else if($_GET['lang'] == "zh")
+            $_SESSION['lang'] = "zh";
+
+    require_once "templates/" . $_SESSION['lang'] . ".php";
+?>
