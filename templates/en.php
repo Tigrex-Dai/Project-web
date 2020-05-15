6930a5f9881593b22a937dbf98730bb0a2d8bcdc
DAI YUQUAN <y.dai@etu.unistra.fr>
2020-05-15 15:09:35 +0200

    Final update

---
+++ templates/en.php
@@ -0,0 +1,27 @@
+<?php
+    $lang = array(
+        "intro1" => "Welcome to our information board,",
+        "intro2" => "make sure you won't miss any activities here!",
+        "bt1" => "To Bottom",
+        "bt2" => "To Top",
+        "bt3" => "To Ernest",
+        "bt4" => "Activities list",
+        "bt5" => "Annonce an event",
+        "pop" => "Here are some popular activities...",
+        "read" => "Read more",
+        "list1" => "Haven't found activities that interest you?",
+        "list2" => "Just hit this button!",
+        "tolist" => "Show all activities",
+        "form1" => "Would like to announce an activity of yourself?",
+        "form2" => "Then hit this button!",
+        "toform" => "Create an announce",
+        "plan" => "New plans here",
+        "addp" => "Add plan",
+        "ptitle1" => "Post a new event",
+        "ptitle2" => "Input your name and e-mail, then the detail of your event.",
+        "bkname" => "Name",
+        "bkmail" => "E-mail",
+        "bkintro" => "Write down your announce with precise date and place.",
+        "act" => "Activities"
+    );
+?>
